576cfa675f8f279703ba5a8a971f4ca1f3e70e3f
Muhammad Umair <umair.nasir014@gmail.com>
2023-04-17 08:36:35 +0200

    web diffs. still poop

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,72 @@
 function scrollToSection(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    
-    targetElement.scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    });
-  }
-  
-  document.querySelectorAll("#navbar a").forEach((navLink) => {
-    navLink.addEventListener("click", scrollToSection);
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute("href");
+  const targetElement = document.querySelector(targetId);
+
+  targetElement.scrollIntoView({
+    behavior: "smooth",
+    block: "start",  
   });
+}
+
+document.querySelectorAll("#navbar a").forEach((navLink) => {
+  navLink.addEventListener("click", scrollToSection);
+});
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  // modal.style.backfaceVisibility="blur"
   
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+// btn.onclick = function (event) {
+//   event.preventDefault();
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
